42fba282875e4bd75c9dd19f3f5ffa99047d21ad
JMS55 <47158642+JMS55@users.noreply.github.com>
2021-04-09 22:49:23 -0400

    Setup routes

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ PARTY TIME
 ## Building and Running
 1. Install [Poetry](https://python-poetry.org/docs/#installation)
 2. `poetry install`
-3. `poetry run python squadify/squadify.py`
+3. `FLASK_ENV=development FLASK_APP=squadify poetry run flask run`
--- squadify/__init__.py
+++ squadify/__init__.py
@@ -1,1 +1,30 @@
-__version__ = '0.1.0'
+__version__ = "0.1.0"
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return "Hello, World!"
+
+
+@app.route("/<uuid:mix_id>")
+def view_mix():
+    return "Hello, World!"
+
+
+@app.route("/api/new", methods=["POST"])
+def new_mix():
+    return "Hello, World!"
+
+
+@app.route("/api/add", methods=["POST"])
+def add_to_mix():
+    return "Hello, World!"
+
+
+@app.route("/api/finish", methods=["POST"])
+def finish_mix():
+    return "Hello, World!"
