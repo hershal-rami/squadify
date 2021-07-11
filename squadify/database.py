954518652c7be2e08e017f98a85384a7ccbc8c83
Justin Hudis <justin.hudis@gmail.com>
2021-07-11 13:11:19 -0400

    Minor bugfix

--- squadify/database.py
+++ squadify/database.py
@@ -3,7 +3,6 @@ from flask import session, abort
 from pymongo import MongoClient
 from spotipy.cache_handler import CacheHandler
 from werkzeug.routing import BaseConverter
-from . import db
 
 
 client = MongoClient("localhost", 27017)
